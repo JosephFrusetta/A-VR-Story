fa12fa1a7a589cf5239a763376d5e1b83bad2929
Joseph Frusetta <JosephFrusetta@gmail.com>
2023-08-27 19:29:20 -0700

    Refactored Button
    Changed the logic to prevent player from waiting too long during button activations and scene changes.

--- SceneChangeBB.cs
+++ SceneChangeBB.cs
@@ -5,19 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeBB : MonoBehaviour
 {
-    public float waitBeforeScene1 = 3.0f;
+    public float waitBeforeNextScene = 3.0f;
     public FadeIn fadeInScript;
     public AudioClip Scene0Audio;
     private AudioSource audioSource;
     private bool buttonActive = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        fadeInScript.StartFadeIn(); // Fades the black UI element so the initial scene becomes visible
+        DynamicGI.UpdateEnvironment(); // Update the Global Illumination - GameObject lighting will update with the new skybox
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = Scene0Audio;
         audioSource.Play();
-
+        StartCoroutine(WaitToActivateButton());
+        
         if (audioSource == null)
         {
             Debug.LogError("No AudioSource component attached to the GameObject.");
@@ -32,25 +35,23 @@ public class SceneChangeBB : MonoBehaviour
 
     public void ButtonActivate()
     {
-        audioSource.Stop(); // Stops all audio from playing
-        fadeInScript.StartFadeIn(); // Fades the black UI element so the initial scene becomes visible
-        StartCoroutine(WaitToActivateButton());  
-
-        if (buttonActive == true)
+        if (buttonActive)
         {
-            StartCoroutine(ChangeScene());  
+            StartCoroutine(ChangeScene());
         }
     }
-
+    
     IEnumerator WaitToActivateButton()
     {
-        yield return new WaitForSeconds(3); // Wait 3 seconds
-        buttonActive = true; // Allow button to be activated after x seconds. This prevents accidental scene selections.
+        yield return new WaitForSeconds(2);
+        buttonActive = true; // Allow button to be activated after 2 seconds.
     }
-
+    
     IEnumerator ChangeScene()
     {
-        yield return new WaitForSeconds(waitBeforeScene1); // Wait before loading the next scene
-        SceneManager.LoadScene("GameScene1"); // Auto load next scene
+        yield return new WaitForSeconds(waitBeforeNextScene); // Wait before loading the next scene
+        audioSource.Stop(); // Stops all audio from playing
+        SceneManager.LoadScene("GameScene1"); // Load next scene
+    }
     }
 }
